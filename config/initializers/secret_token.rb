eeef4fff55e4324b366947b02c589b3c00ef542c
Lauren <lconrad@proteuseng.com>
2012-05-14 14:38:43 -0400

    Did new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outsmart::Application.config.secret_token = 'db890c3ae9f5af12ce16a60fe768393c8024f9dc0b5c953744a3e503ceeabceafba5f5e57f2e45ceb01703a0e925d00a2089a5a53e6cf6ca89fe363cfb5007cc'
