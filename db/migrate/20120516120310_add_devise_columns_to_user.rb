fc1139c1d41603d074c0cd885cd509a12eca3325
Lauren <lconrad@proteuseng.com>
2012-05-16 08:09:26 -0400

    Add AddDeviseColumn migration

---
+++ db/migrate/20120516120310_add_devise_columns_to_user.rb
@@ -0,0 +1,13 @@
+class AddDeviseColumnsToUser < ActiveRecord::Migration
+  def change
+ #   change_table :users do |t|
+ #     t.string :encrypted_password, :null => false, :default => '', :limit => 128
+ #     t.confirmable
+ #     t.recoverable
+ #     t.rememberable
+ #     t.trackable
+ #     t.token_authenticatable
+ #     t.timestamps
+ #   end
+  end
+end
