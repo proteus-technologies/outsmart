a306130c154bcd04ac3d937d69d4dd375bdf0a2e
Lauren <lconrad@proteuseng.com>
2012-05-16 08:17:35 -0400

    Add devise code to AddDeviseColumnsToUser

--- db/migrate/20120516120310_add_devise_columns_to_user.rb
+++ db/migrate/20120516120310_add_devise_columns_to_user.rb
@@ -1,13 +1,13 @@
 class AddDeviseColumnsToUser < ActiveRecord::Migration
   def change
- #   change_table :users do |t|
- #     t.string :encrypted_password, :null => false, :default => '', :limit => 128
- #     t.confirmable
- #     t.recoverable
- #     t.rememberable
- #     t.trackable
- #     t.token_authenticatable
- #     t.timestamps
- #   end
+    change_table :users do |t|
+      t.string :encrypted_password, :null => false, :default => '', :limit => 128
+      t.confirmable
+      t.recoverable
+      t.rememberable
+      t.trackable
+      t.token_authenticatable
+      t.timestamps
+    end
   end
 end
