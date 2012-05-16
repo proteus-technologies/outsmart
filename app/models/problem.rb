945be0c75e893622c175901aa05a12026ec6b955
Lauren <lconrad@proteuseng.com>
2012-05-16 13:11:44 -0400

    Added problems list to Admin page

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,3 +1,5 @@
 class Problem < ActiveRecord::Base
   attr_accessible :end_date, :start_date, :text_description, :title, :user_id
+  has_many :solutions
+  
 end
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,3 +1,4 @@
 class Solution < ActiveRecord::Base
   attr_accessible :problem_ID, :solution_file_url, :text_description, :user_ID
+  belongs_to :problem
 end
