d4b8487e1640ffc035ffa445d5a46b61c763cc1f
mkalkbrenner <mkalkbrenner@proteuseng.com>
2012-05-16 13:25:18 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Outsmart::Application.routes.draw do
   get "static_pages/admin"
 
-  get "static_pages/home"
-
   resources :problems
 
   resources :solutions
@@ -58,7 +56,7 @@ Outsmart::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
