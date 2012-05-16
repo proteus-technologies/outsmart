db6a714902b3717529bcf5ed833412f74a44af9a
Lauren <lconrad@proteuseng.com>
2012-05-16 08:45:20 -0400

    deleted a topic branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515174937) do
+ActiveRecord::Schema.define(:version => 20120516120310) do
 
   create_table "problems", :force => true do |t|
     t.string   "title"
