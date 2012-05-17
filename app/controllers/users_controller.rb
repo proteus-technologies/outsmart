f02fa3a5e88fc31a64f2b6a6d220735cd3f94f94
Lauren <lconrad@proteuseng.com>
2012-05-17 11:28:02 -0400

    Add redirect code to User controller but commented out (uncomment when login is working)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+ # before_filter :require_login, :except => [:new, :create, :sign_in]
+
+  # Require login to access the Problem arena (flash won't show
+  # due to being redirected)
+  def require_login
+    unless user_signed_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to root_path
+    end
+  end
   # GET /users
   # GET /users.json
   def index
