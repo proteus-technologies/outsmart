e6f813631c0e24cc9ca3658ac8d90e849843a9e4
Lauren <lconrad@proteuseng.com>
2012-05-18 15:33:07 -0400

    Try to remove User but failing

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :Is_Admin, :Problem_ID, :Solution_ID, :User_ID, :creation_date, :email, :name, :password, :phone, :updated_date
-  has_many :problems
-  has_many :solutions
-  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
-  
-end
-
