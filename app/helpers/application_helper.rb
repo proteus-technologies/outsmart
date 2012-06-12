b40ad7f09eab1f976e8904d2295e0fc6bb9457c2
Lauren <lconrad@proteuseng.com>
2012-06-12 12:36:42 -0400

    Added Redcarpet Markdown for creating Problems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,6 @@ module ApplicationHelper
     #options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
     #Redcarpet.new(text, *options).to_html.html_safe
     #Redcarpet.new(text).to_html.html_safe
+    #Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :hard_wrap => true)
   #end
 end
